cfd558e3994bdac5b86b10cf9ff9701aad32643f
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2020-12-08 23:03:58 -0500

    api response added and user input stored in local storage

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 var apiKey = "abedb9d601e3b4828d2d985b7119ff3c";
-var city = 'Cincinnati'
-var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`
+var city = $('#city-input').val()
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "city&units=metric&APPID=" + apiKey;
+var submitBtn = $('#submit')
 
-$.ajax({
-    url: queryURL,
-    method: 'GET',
-}).then(function(response) {
-    console.log(response)
-})
-
-
-
-
-
-
-
-
-
-
+function getWeather(){
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+    }).then(function(response) {
+        console.log(response)
+        var weatherInfo = {
+            temperature: response.main.temp,
+            humidity: response.main.humidity,
+            wind: response.wind.speed
+        }
+    });
+};
 
 
+$('#submit').on('click', function(event) {
 
+    getWeather()
+    event.preventDefault()
 
-
-
-
-
+    var key = 'city';
+    var value = $('#city-input').val();
+    if (key && value) {
+        localStorage.setItem(key, value) 
+    }
+    
+});
 
 
