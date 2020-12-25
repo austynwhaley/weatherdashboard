52f0478ff683a89ba96cdb2df435829433769e9a
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2020-12-25 18:13:58 -0500

    js updated for mutiple ajax calls to be made

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var apiKey = "2a150efcdc8fd6427901a38f082e72cc";
-var city = 'Cincinnati'
-var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
-var submitBtn = $('#submit')
+var city = 'Boston'
+var submitBtn = $('#submitBtn')
 
 function getWeather(){
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
     $.ajax({
         url: queryURL,
         method: 'GET',
@@ -13,19 +13,32 @@ function getWeather(){
             name: response.name,
             temperature: response.main.temp,
             humidity: response.main.humidity,
-            wind: response.wind.speed
+            wind: response.wind.speed,
+            lat: response.coord.lat,
+            lon: response.coord.lon
         }
 
+
         $('#currentWeatherName').text(searchInfo.name);
+        $('#temperature').text("Temperature: " + searchInfo.temperature + "°C");
+        $('#humidity').text("Humidity: " + searchInfo.humidity + "%");
+        $('#windSpeed').text("Wind Speed: " + searchInfo.wind + "m/s");
     });
 };
 
+function getUvIndex(){
+    var queryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apikey}`
+}
+
+function getForcast(){
+    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`
+}
 
 
 
-$('#submit').on('click', function(event) {
+$('#submitBtn').on('click', function(event) {
 
-    getWeather()
+    getWeather(city)
     event.preventDefault()
 
     var key = 'city';
@@ -35,7 +48,3 @@ $('#submit').on('click', function(event) {
     }
     
 });
-
-
-
-
