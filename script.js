0f4790849b6c274c6ff7039f5eb36acaee9d74f6
Austyn Whaley <70417091+austynwhaley@users.noreply.github.com>
2020-12-27 21:23:58 -0500

    uv index ajax call added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var apiKey = "2a150efcdc8fd6427901a38f082e72cc";
 var city = 'Boston'
 var submitBtn = $('#submitBtn')
+var lat = '42.36';
+var lon = '-71.06';
 
 function getWeather(){
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
@@ -8,7 +10,7 @@ function getWeather(){
         url: queryURL,
         method: 'GET',
     }).then(function(response) {
-        console.log(response)
+        
         var searchInfo = {
             name: response.name,
             temperature: response.main.temp,
@@ -27,24 +29,58 @@ function getWeather(){
 };
 
 function getUvIndex(){
-    var queryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apikey}`
-}
+    var queryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+    }).then(function(response) {
+        
+        let currUVIndex = response.value;
+        let uvSeverity = "green";
+        let textColor = "white"
+        
+        if (currUVIndex >= 11) {
+            uvSeverity = "purple";
+        } else if (currUVIndex >= 8) {
+            uvSeverity = "red";
+        } else if (currUVIndex >= 6) {
+            uvSeverity = "orange";
+            textColour = "black"
+        } else if (currUVIndex >= 3) {
+            uvSeverity = "yellow";
+            textColour = "black"
+        }
+
+        $('#uvIndex').append(`UV Index: <span class="text-${textColor} uvPadding" style="background-color: ${uvSeverity};">${currUVIndex}</span></p>`)
+    });
+};
 
 function getForcast(){
-    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`
-}
+    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+    }).then(function(response) {
+        console.log(response)
+    });
+};
 
 
 
 $('#submitBtn').on('click', function(event) {
 
-    getWeather(city)
-    event.preventDefault()
+    
+    event.preventDefault();
+    getWeather();
+    getUvIndex();
+    getForcast();
 
     var key = 'city';
     var value = $('#city-input').val();
     if (key && value) {
         localStorage.setItem(key, value) 
-    }
+    };
     
 });
